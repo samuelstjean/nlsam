06098db6899be7bcb188274562a5bff1eb47bbea
Samuel St-Jean <stjeansam@gmail.com>
2015-01-02 15:17:52 -0500

    setup change for travis testing

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from Cython.Distutils import build_ext
 import cython_gsl
 
 ext = [Extension('nlsam.stabilizer',
-               ['nlsam/stabilizer.pyx'],
-               libraries=cython_gsl.get_libraries(),
-               library_dirs=[cython_gsl.get_library_dir()],
-               cython_include_dirs=[cython_gsl.get_cython_include_dir()])]
+                ['nlsam/stabilizer.pyx'],
+                libraries=cython_gsl.get_libraries(),
+                library_dirs=[cython_gsl.get_library_dir()],
+                cython_include_dirs=[cython_gsl.get_cython_include_dir()])]
 
 setup(
     name = 'gsl hyp1f1',
