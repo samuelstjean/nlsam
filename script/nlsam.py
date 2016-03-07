af57e996f5a382c51c1c861cc506234995ffa5f3
Samuel St-Jean <samuel.st-jean@usherbrooke.ca>
2016-03-07 11:42:51 +0100

    update nlsam.py with caller.py

--- script/nlsam.py
+++ script/nlsam.py
@@ -3,12 +3,15 @@
 
 from __future__ import division, print_function
 
+import os
+# Fix openblas threading bug with openmp before loading numpy
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+
+import argparse
+from multiprocessing import cpu_count
+
 import nibabel as nib
 import numpy as np
-
-import os
-import argparse
-
 
 from nlsam.denoiser import denoise, greedy_set_finder
 from nlsam.angular_tools import angular_neighbors
@@ -16,17 +19,13 @@ from nlsam.smoothing import local_standard_deviation
 
 from dipy.io.gradients import read_bvals_bvecs
 
-from time import time
-from copy import copy
-from ast import literal_eval
-from multiprocessing import cpu_count
-
 
 DESCRIPTION = """
     Convenient script to call the denoising dictionary learning/sparse coding
     functions. It enables the user to select a whole range of parameters to
     test instead of relying on scripts that call the relevant scripts.
     """
+
 
 def buildArgsParser():
 
@@ -44,7 +43,7 @@ def buildArgsParser():
     p.add_argument('bvecs', action='store', metavar='bvecs',
                    help='Path of the bvecs file, in FSL format.')
 
-    p.add_argument('-sigma', action='store', required=False, type=str,
+    p.add_argument('-sigma', action='store', required=True, type=str,
                    help='Path to standard deviation volume.')
 
     p.add_argument('-D', action='store', metavar='D',
@@ -52,11 +51,6 @@ def buildArgsParser():
                    help='Path to a prelearned dictionnary D in npy format to \
                    use for the sparse coding step. Supplying D will skip the \
                    dictionnary learning part.')
-
-    p.add_argument('-overlap', action='store', metavar='overlap',
-                   required=False, default=None, type=str,
-                   help='Specifies overlap between blocks, ranging from 0 \
-                   (no overlap) to min(block_size)-1 (full overlap)')
 
     p.add_argument('-no_whitening', action='store_false', required=False,
                    help='If True, do not apply ZCA whitening. Each block \
@@ -117,20 +111,15 @@ def buildArgsParser():
                    metavar='savename', required=True, type=str,
                    help='Path and prefix for the saved denoised file.')
 
-    p.add_argument('-mask', action='store', dest='mask_data',
+    p.add_argument('-mask', action='store', dest='mask',
                    metavar='', required=False, default=None, type=str,
                    help='Path to a binary mask. Only the data inside the mask \
                    will be reconstructed by the sparse coding algorithm.')
 
-    p.add_argument('--no_subsampling', dest='no_subsampling', action='store_true',
-                   default=False, required=False,
-                   help='If supplied, process all volumes multiple time, as opposed to ' +
-                   'only at least once.')
-
     p.add_argument('--no_symmetry', dest='no_symmetry', action='store_true',
                    default=False, required=False,
                    help='If supplied, assumes the set of bvals/bvecs to already be symmetrized, ' +
-                   'i.e. All points (x,y,z) on the sphere and (-x,-y,-z) were acquired.')
+                   'i.e. All points (x,y,z) on the sphere and (-x,-y,-z) were acquired, like in DSI.')
     return p
 
 
@@ -145,28 +134,18 @@ def main():
 
     vol = nib.load(args.input)
     data = vol.get_data()
+    vol.uncache()
     affine = vol.get_affine()
 
-    use_abs = False
-    use_clip = False
+    sigma = nib.load(args.sigma).get_data()**2
 
-    if args.no_subsampling:
-        greedy_subsampler = False
-    else:
-        greedy_subsampler = True
-
-    crop = False
+    greedy_subsampler = True
+    implausible_signal_boost = True
     n_iter = 10
-    implausible_signal_hack = True
     debug = False
 
-    if use_abs:
-        data = np.abs(data)
-    elif use_clip:
-        data[data < 0] = 0
-
     original_dtype = data.dtype
-
+    data = data.astype(np.float64)
     block_size = np.array((3, 3, 3, int(args.block_size)))
     param_D = {}
     param_alpha = {}
@@ -174,11 +153,6 @@ def main():
     if len(block_size) != len(data.shape):
         raise ValueError('Block shape and data shape are not of the same \
                          dimensions', data.shape, block_size.shape)
-
-    if args.overlap is not None:
-        overlap = np.array(literal_eval(args.overlap))
-    else:
-        overlap = np.ones(len(block_size), dtype='int16')
 
     if args.cores is None:
         param_D['numThreads'] = cpu_count()
@@ -201,18 +175,18 @@ def main():
         param_alpha['D'] = np.load(args.D)
 
     param_alpha['mode'] = args.mode_alpha
+    param_alpha['pos'] = args.pos_alpha
 
     param_D['mode'] = args.mode_D
     param_D['iter'] = args.iter
     param_D['K'] = args.nb_atoms_D
     param_D['posD'] = args.pos_D
-    param_alpha['pos'] = args.pos_alpha
     param_D['posAlpha'] = args.pos_alpha
 
-    if args.mask_data is not None:
-        mask_train = nib.load(args.mask_data).get_data().squeeze().astype(np.bool)
+    if args.mask is not None:
+        mask = nib.load(args.mask).get_data().astype(np.bool)
     else:
-        mask_train = None
+        mask = np.ones(data.shape[:-1], dtype=np.bool)
 
     filename = args.savename
 
@@ -254,51 +228,21 @@ def main():
 
     neighbors = (angular_neighbors(sym_bvecs, block_size[-1] - num_b0s) % (data.shape[-1] - num_b0s))[:data.shape[-1] - num_b0s]
 
-    if args.mask_data is not None:
-        mask_data = nib.load(args.mask_data).get_data().squeeze().astype(np.bool)
-        mask = nib.load(args.mask_data).get_data().squeeze().astype(np.bool)
-    else:
-
-        mask = np.ones_like(data[..., 0], dtype=np.bool)
-        mask_data = mask
-
-        if debug:
-            nib.save(nib.Nifti1Image(data, affine), filename + '_masked.nii.gz')
-            nib.save(nib.Nifti1Image(mask.astype('int8'), affine), filename + '_mask.nii.gz')
-            # nib.save(nib.Nifti1Image(mask_noise.astype('int8'), affine), filename + '_mask_noise.nii.gz')
-
-    if args.sigma is not None:
-        sigma = nib.load(args.sigma).get_data()**2
-        print("Found sigma volume! Using", args.sigma, "as the noise standard deviation")
-    else:
-        print("No volume found for noise estimation, using local variance internally for bounding the reconstruction error")
-        sigma = local_standard_deviation(data)**2
-        nib.save(nib.Nifti1Image(sigma,np.eye(4)), filename + "_variance.nii.gz")
-
     # Always abs b0s, as it makes absolutely no sense physically not to
     print(np.sum(data[..., b0_loc] < 0), "b0s voxel < 0")
-    nib.save(nib.Nifti1Image((data[..., b0_loc] < 0).astype(np.int16), np.eye(4)),'implausible_voxels.nii.gz')
+    nib.save(nib.Nifti1Image((data[..., b0_loc] < 0).astype(np.int16), np.eye(4)), 'implausible_voxels.nii.gz')
     # Implausible signal hack
     print("Number of implausible signal", np.sum(data[..., b0_loc] < data))
-    if implausible_signal_hack:
+    if implausible_signal_boost:
         data[..., b0_loc] = np.max(data, axis=-1, keepdims=True)
     print("Number of implausible signal after hack", np.sum(data[..., b0_loc] < data))
 
-    nib.save(nib.Nifti1Image(data[..., b0_loc], np.eye(4)),'max_b0s_voxels.nii.gz')
+    nib.save(nib.Nifti1Image(data[..., b0_loc], np.eye(4)), 'max_b0s_voxels.nii.gz')
 
     orig_shape = data.shape
     new_block_size = 3
 
-    print("Choosing new full block size, now", new_block_size, "was", block_size)
     block_size = [new_block_size, new_block_size, new_block_size, block_size[-1]]
-
-    print("overlap is", overlap)
-    mask = mask_data
-
-    full_block = np.append(block_size[:-1], data.shape[-1])
-    padded_shape = data.shape
-
-    new_block_size = 3
 
     print("Choosing new  block size, now", new_block_size, "was", block_size)
     block_size = [new_block_size, new_block_size, new_block_size, block_size[-1]]
@@ -317,26 +261,19 @@ def main():
         indexes += [(i,) + tuple(neighbors[i])]
 
     if greedy_subsampler:
-        print("Greedy subsampler hack is on", len(indexes))
         indexes = greedy_set_finder(indexes)
-        print("Number of indexes is now", len(indexes))
-
-    if debug:
-        nib.save(nib.Nifti1Image(data_neighbors, affine),
-                 filename + '_neighbors.nii.gz')
 
     b0_block_size = tuple(block_size[:-1]) + ((block_size[-1] + num_b0s,))
-    print(b0_block_size, block_size[:-1], tuple((block_size[-1] + num_b0s,)), block_size[-1], num_b0s)
 
-    nbiter = 150
+    nbiter = 50
     K = np.asscalar(np.prod(b0_block_size))
 
     param_D['iter'] = nbiter
-    param_D['K'] = K  # // 2
-    alpha_ml = 0.3
-    print("param run", K, nbiter, alpha_ml)
-    param_alpha['lambda1'] = alpha_ml
-    param_D['lambda1'] = alpha_ml
+    param_D['K'] = K
+    # alpha_ml = 0.3
+    # print("param run", K, nbiter, alpha_ml)
+    # param_alpha['lambda1'] = alpha_ml
+    # param_D['lambda1'] = alpha_ml
     denoised_shape = data.shape[:-1] + (neighbors_shape[-1],)
     denoised_shape = data.shape[:-1] + (data.shape[-1] + num_b0s,)
     data_denoised = np.zeros(denoised_shape, np.float64)
@@ -374,14 +311,16 @@ def main():
         # Stack the rest in-between
         rest_of_b0s = rest_of_b0s[:-number_of_end_b0s]
         b0_denoised = np.squeeze(data_denoised[..., b0_loc])
+        data_denoised_insert = np.empty(orig_shape, data_denoised.dtype)
+        n = 0
+        for i in range(orig_shape[-1]):
+            if i in rest_of_b0s:
+                data_denoised_insert[..., i] = b0_denoised
+                n += 1
+            else:
+                data_denoised_insert[..., i] = data_denoised[..., i - n]
 
-        for idx in rest_of_b0s:
-            data_denoised = np.insert(data_denoised, idx, b0_denoised, axis=-1)
-
-    if use_abs:
-        data_denoised = np.abs(data_denoised)
-    elif use_clip:
-        data_denoised[data_denoised < 0] = 0
+        data_denoised = data_denoised_insert
 
     nib.save(nib.Nifti1Image(data_denoised.astype(original_dtype), affine), filename)
 
