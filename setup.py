10b02db383376aa2a387b3145cf9c94d60bacbef
Samuel St-Jean <samuel@isi.uu.nl>
2016-03-12 10:47:24 +0100

    edited setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,8 @@ params['deps'] = ['dipy>=0.11',
                   'scipy>=0.12',
                   'nibabel>=1.3']
 # params['links'] = ['https://github.com/samuelstjean/spams-python/releases/tag/0.1#egg=spams-2.5']
-params['links'] = ['https://github.com/samuelstjean/spams-python/releases/download/0.1/spams-python-v2.5-svn2014-07-04.tar.gz#egg=spams-2.5']
+# params['links'] = ['https://github.com/samuelstjean/spams-python/releases/download/0.1/spams-python-v2.5-svn2014-07-04.tar.gz#egg=spams-2.5']
+params['links'] = ['git+https://github.com/samuelstjean/spams-python/releases/download/0.1/spams-python-v2.5-svn2014-07-04.tar.gz#egg=spams-2.5']
 # https://github.com/samuelstjean/spams-python/releases/tag/0.1
 #/0.1/spams-python-v2.5-svn2014-07-04.tar.gz']
 
