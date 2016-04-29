defc6648bbf42ea01c5d5ed8ad7ef24894830af8
Samuel St-Jean <samuel.st-jean@usherbrooke.ca>
2016-04-29 15:59:39 +0200

    added freeze support for windows

--- build_stuff/linux/run_build.sh
+++ build_stuff/linux/run_build.sh
@@ -5,4 +5,3 @@ python setup.py build_ext -i --force
 pyinstaller build_stuff/linux/nlsam.spec --onefile
 pyinstaller build_stuff/linux/stabilizer.spec --onefile
 zip -j nlsam_linux_x64.zip dist/nlsam dist/stabilizer LICENSE README.md CHANGELOG.md
-s
