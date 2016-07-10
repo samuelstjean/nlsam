cc76971bf9ac7207d73d60f5d6a33e47fc2531c5
Samuel St-Jean <samuel@isi.uu.nl>
2016-07-10 16:04:19 +0200

    more doc and misc fixes in denoiser.py

--- nlsam/denoiser.py
+++ nlsam/denoiser.py
@@ -20,31 +20,30 @@ except ImportError:
     raise ValueError("Couldn't find spams library, is the package correctly installed?")
 
 
-def nlsam_denoise(data, sigma, bvals, bvecs, block_size, mask=None, no_symmetry=False, n_cores=None, greedy_subsampler=True, n_iter=10,
-                  b0_thresh=10):
+def nlsam_denoise(data, sigma, bvals, bvecs, block_size,
+                  mask=None, no_symmetry=False, n_cores=None,
+                  greedy_subsampler=True, n_iter=10, b0_thresh=10):
     """Main nlsam denoising function which sets up everything nicely for the local
     block denoising.
 
     Input
     -----------
     data : ndarray
-
-
+        Input volume to denoise.
     sigma : ndarray
-        Noise standard deviation estimation for each voxel.
+        Noise standard deviation estimation at each voxel.
         Converted to variance internally.
-    bvals : ndarray
-    bvecs : ndarray
-
+    bvals : 1D array
+        the N b-values associated to each of the N diffusion volume.
+    bvecs : N x 3 2D array
+        the N 3D vectors for each acquired diffusion gradients.
     block_size : int
         Number of angular neighbors to process at once as similar data.
 
     Optional parameters
     -------------------
-
     mask : ndarray, default None
         Retrict computations to voxels inside the mask to reduce runtime.
-
     no_symmetry : bool, default False
         If True, assumes that for each coordinate (x, y, z) in bvecs,
         (-x, -y, -z) was also acquired.
@@ -110,9 +109,6 @@ def nlsam_denoise(data, sigma, bvals, bvecs, block_size, mask=None, no_symmetry=
 
     neighbors = (angular_neighbors(sym_bvecs, block_size[-1] - num_b0s) % (data.shape[-1] - num_b0s))[:data.shape[-1] - num_b0s]
 
-    # if implausible_signal_boost:
-    #     data[..., b0_loc] = np.max(data, axis=-1, keepdims=True)
-
     orig_shape = data.shape
 
     # Full overlap for dictionary learning
@@ -145,12 +141,11 @@ def nlsam_denoise(data, sigma, bvals, bvecs, block_size, mask=None, no_symmetry=
         data_denoised[..., b0_loc + dwi_idx] += local_denoise(to_denoise,
                                                               b0_block_size,
                                                               overlap,
-                                                              param_alpha,
-                                                              param_D,
                                                               variance,
-                                                              n_iter,
-                                                              mask,
-                                                              dtype=np.float64)
+                                                              n_iter=n_iter,
+                                                              mask=mask,
+                                                              dtype=np.float64,
+                                                              n_cores=n_cores)
 
     divider = np.bincount(np.array(indexes, dtype=np.int16).ravel())
     divider = np.insert(divider, b0_loc, len(indexes))
@@ -177,6 +172,88 @@ def nlsam_denoise(data, sigma, bvals, bvecs, block_size, mask=None, no_symmetry=
         data_denoised = data_denoised_insert
 
     return data_denoised
+
+
+def local_denoise(data, block_size, overlap, variance, n_iter=10, mask=None,
+                  dtype=np.float64, n_cores=None):
+
+    if mask is None:
+        mask = np.ones(data.shape[:-1], dtype=np.bool)
+
+    # no overlapping blocks for training
+    # no_over = (0, 0, 0, 0)
+    no_over = np.zeros(data.ndim)
+    X = im2col_nd(data, block_size, no_over)
+
+    # Solving for D
+    param_alpha = {}
+    param_alpha['pos'] = True
+    param_alpha['mode'] = 1
+    # param_alpha['lambda1'] = 1.2 / np.sqrt(np.prod(block_size))
+
+    param_D = {}
+    param_D['verbose'] = False
+    param_D['posAlpha'] = True
+    param_D['posD'] = True
+    param_D['mode'] = 2
+    param_D['lambda1'] = 1.2 / np.sqrt(np.prod(block_size))
+    param_D['K'] = int(2 * np.prod(block_size))
+    param_D['iter'] = 150
+    param_D['batchsize'] = 500
+
+    if 'D' in param_alpha:
+        param_D['D'] = param_alpha['D']
+
+    # mask_col = im2col_nd(np.broadcast_to(mask[..., None], data.shape), block_size, no_over)
+    mask_col = im2col_nd(mask, block_size[:-1], overlap[:-1])
+    train_idx = np.sum(mask_col, axis=0) > (mask_col.shape[0] / 2.)
+
+    train_data = X[:, train_idx]
+    train_data = np.asfortranarray(train_data[:, np.any(train_data != 0, axis=0)], dtype=dtype)
+    train_data /= np.sqrt(np.sum(train_data**2, axis=0, keepdims=True), dtype=dtype)
+    param_alpha['D'] = spams.trainDL(train_data, **param_D)
+    param_alpha['D'] /= np.sqrt(np.sum(param_alpha['D']**2, axis=0, keepdims=True, dtype=dtype))
+    param_D['D'] = param_alpha['D']
+
+    del train_data, X
+
+    # n_cores = param_alpha['numThreads']
+    param_alpha['numThreads'] = 1
+    param_D['numThreads'] = 1
+
+    time_multi = time()
+    pool = Pool(processes=n_cores)
+
+    arglist = [(data[:, :, k:k+block_size[2]], mask[:, :, k:k+block_size[2]], variance[:, :, k:k+block_size[2]], block_size_subset, overlap_subset, param_alpha_subset, param_D_subset, dtype_subset, n_iter_subset)
+               for k, block_size_subset, overlap_subset, param_alpha_subset, param_D_subset, dtype_subset, n_iter_subset
+               in zip(range(data.shape[2] - block_size[2] + 1),
+                      repeat(block_size),
+                      repeat(overlap),
+                      repeat(param_alpha),
+                      repeat(param_D),
+                      repeat(dtype),
+                      repeat(n_iter))]
+
+    data_denoised = pool.map(processer, arglist)
+    pool.close()
+    pool.join()
+
+    # param_alpha['numThreads'] = n_cores
+    # param_D['numThreads'] = n_cores
+
+    print('Multiprocessing done in {0:.2f} mins.'.format((time() - time_multi) / 60.))
+
+    # Put together the multiprocessed results
+    data_subset = np.zeros_like(data, dtype=np.float32)
+    divider = np.zeros_like(data, dtype=np.int16)
+    ones = np.ones_like(data_denoised[0], dtype=np.int16)
+
+    for k in range(len(data_denoised)):
+        data_subset[:, :, k:k + block_size[2]] += data_denoised[k]
+        divider[:, :, k:k + block_size[2]] += ones
+
+    data_subset /= divider
+    return data_subset
 
 
 def greedy_set_finder(sets):
@@ -214,18 +291,19 @@ def processer(arglist):
     return _processer(data, mask, variance, block_size, overlap, param_alpha, param_D, dtype=dtype, n_iter=n_iter)
 
 
-def _processer(data, mask, variance, block_size, overlap, param_alpha, param_D, dtype=np.float64, n_iter=10, gamma=3., tau=1.):
+def _processer(data, mask, variance, block_size, overlap, param_alpha, param_D,
+               dtype=np.float64, n_iter=10, gamma=3., tau=1., tolerance=1e-5):
 
     orig_shape = data.shape
-    mask_array = im2col_nd(mask, block_size[:3], overlap[:3])
-    train_idx = np.sum(mask_array, axis=0) > mask_array.shape[0] / 2.
+    mask_array = im2col_nd(mask, block_size[:-1], overlap[:-1])
+    train_idx = np.sum(mask_array, axis=0) > (mask_array.shape[0] / 2.)
 
     # If mask is empty, return a bunch of zeros as blocks
     if not np.any(train_idx):
         return np.zeros_like(data)
 
     X = im2col_nd(data, block_size, overlap)
-    var_mat = np.median(im2col_nd(variance[..., 0:orig_shape[-1]], block_size, overlap)[:, train_idx], axis=0).astype(dtype)
+    var_mat = im2col_nd(variance, block_size[:-1], overlap[:-1])[:, train_idx]
     X_full_shape = X.shape
     X = X[:, train_idx]
 
@@ -245,8 +323,8 @@ def _processer(data, mask, variance, block_size, overlap, param_alpha, param_D, 
 
     xi = np.random.randn(X.shape[0], X.shape[1]) * var_mat
     eps = np.max(np.abs(np.dot(D.T, xi)), axis=0)
-    param_alpha['mode'] = 1
-    param_alpha['pos'] = True
+    # param_alpha['mode'] = 1
+    # param_alpha['pos'] = True
 
     for _ in range(n_iter):
         not_converged = np.equal(has_converged, False)
@@ -262,7 +340,7 @@ def _processer(data, mask, variance, block_size, overlap, param_alpha, param_D, 
         arr = alpha.toarray()
         nonzero_ind = arr != 0
         arr[nonzero_ind] /= W[nonzero_ind]
-        has_converged = np.max(np.abs(alpha_old - arr), axis=0) < 1e-5
+        has_converged = np.max(np.abs(alpha_old - arr), axis=0) < tolerance
 
         if np.all(has_converged):
             break
@@ -278,78 +356,3 @@ def _processer(data, mask, variance, block_size, overlap, param_alpha, param_D, 
     X2[:, train_idx] = X
 
     return col2im_nd(X2, block_size, orig_shape, overlap, weigths)
-
-
-def local_denoise(data, block_size, overlap, param_alpha, param_D, variance, n_iter=10,
-                  mask=None, dtype=np.float64):
-
-    # no overlapping blocks for training
-    no_over = (0, 0, 0, 0)
-    X = im2col_nd(data, block_size, no_over)
-
-    # Solving for D
-    param_alpha['pos'] = True
-    param_alpha['mode'] = 2
-    param_alpha['lambda1'] = 1.2 / np.sqrt(np.prod(block_size))
-
-    param_D['verbose'] = False
-    param_D['posAlpha'] = True
-    param_D['posD'] = True
-    param_D['mode'] = 2
-    param_D['lambda1'] = 1.2 / np.sqrt(np.prod(block_size))
-    param_D['K'] = int(2*np.prod(block_size))
-    param_D['iter'] = 150
-    param_D['batchsize'] = 500
-
-    if 'D' in param_alpha:
-        param_D['D'] = param_alpha['D']
-
-    mask_col = im2col_nd(np.broadcast_to(mask[..., None], data.shape), block_size, no_over)
-    train_idx = np.sum(mask_col, axis=0) > mask_col.shape[0]/2
-
-    train_data = X[:, train_idx]
-    train_data = np.asfortranarray(train_data[:, np.any(train_data != 0, axis=0)], dtype=dtype)
-    train_data /= np.sqrt(np.sum(train_data**2, axis=0, keepdims=True), dtype=dtype)
-    param_alpha['D'] = spams.trainDL(train_data, **param_D)
-    param_alpha['D'] /= np.sqrt(np.sum(param_alpha['D']**2, axis=0, keepdims=True, dtype=dtype))
-    param_D['D'] = param_alpha['D']
-
-    del train_data
-
-    n_cores = param_alpha['numThreads']
-    param_alpha['numThreads'] = 1
-    param_D['numThreads'] = 1
-
-    time_multi = time()
-    pool = Pool(processes=n_cores)
-
-    arglist = [(data[:, :, k:k+block_size[2]], mask[:, :, k:k+block_size[2]], variance[:, :, k:k+block_size[2]], block_size_subset, overlap_subset, param_alpha_subset, param_D_subset, dtype_subset, n_iter_subset)
-               for k, block_size_subset, overlap_subset, param_alpha_subset, param_D_subset, dtype_subset, n_iter_subset
-               in zip(range(data.shape[2] - block_size[2] + 1),
-                      repeat(block_size),
-                      repeat(overlap),
-                      repeat(param_alpha),
-                      repeat(param_D),
-                      repeat(dtype),
-                      repeat(n_iter))]
-
-    data_denoised = pool.map(processer, arglist)
-    pool.close()
-    pool.join()
-
-    param_alpha['numThreads'] = n_cores
-    param_D['numThreads'] = n_cores
-
-    print('Multiprocessing done in {0:.2f} mins.'.format((time() - time_multi) / 60.))
-
-    # Put together the multiprocessed results
-    data_subset = np.zeros_like(data, dtype=np.float32)
-    divider = np.zeros_like(data, dtype=np.int16)
-    ones = np.ones_like(data_denoised[0], dtype=np.int16)
-
-    for k in range(len(data_denoised)):
-        data_subset[:, :, k:k+block_size[2]] += data_denoised[k]
-        divider[:, :, k:k+block_size[2]] += ones
-
-    data_subset /= divider
-    return data_subset
