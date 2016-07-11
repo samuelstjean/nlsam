b43185f9f9909d9fbb68cfd49770fdc2c59079f5
Samuel St-Jean <samuel@isi.uu.nl>
2016-07-11 21:16:26 +0200

    crop example for faster test

--- nlsam/tests/test_scripts.sh
+++ nlsam/tests/test_scripts.sh
@@ -10,6 +10,10 @@ function check_return_code()
         exit 1
     fi
 }
+
+# Crop example dataset
+python -c 'import nibabel as nib; import numpy as np; d = nib.load("dwi.nii.gz").get_data(); nib.save(nib.Nifti1Image(d[70:80],np.eye(4)), "dwi.nii.gz")'
+
 # Test on example dataset
 stabilizer dwi.nii.gz dwi_stab_localstd.nii.gz 1 sigma_localstd.nii.gz -m mask.nii.gz --bvals bvals --bvecs bvecs --noise_est local_std --smooth no_smoothing
 check_return_code $?
