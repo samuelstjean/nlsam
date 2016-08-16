288c30b242f2ea771a3e81ff269dc108adf81095
Samuel St-Jean <samuel.st-jean@usherbrooke.ca>
2016-08-16 16:35:25 +0200

    travis python 3

--- nlsam/tests/test_scripts.sh
+++ nlsam/tests/test_scripts.sh
@@ -12,8 +12,9 @@ check_return_code ()
 }
 
 # Crop example dataset
-python -c 'import nibabel as nib; import numpy as np; d = nib.load("dwi.nii.gz").get_data(); nib.save(nib.Nifti1Image(d,np.eye(4)), "dwi_crop.nii.gz")'
-python -c 'import nibabel as nib; import numpy as np; d = nib.load("mask.nii.gz").get_data(); nib.save(nib.Nifti1Image(d,np.eye(4)), "mask_crop.nii.gz")'
+python -c 'import nibabel as nib; import numpy as np; d = nib.load("dwi.nii.gz").get_data(); nib.save(nib.Nifti1Image(d[40:60],np.eye(4)), "dwi_crop.nii.gz")'
+python -c 'import nibabel as nib; import numpy as np; d = nib.load("mask.nii.gz").get_data(); nib.save(nib.Nifti1Image(d[40:60],np.eye(4)), "mask_crop.nii.gz")'
+python -c 'import nibabel as nib; import numpy as np; d = nib.load("mask.nii.gz").get_data(); nib.save(nib.Nifti1Image(np.abs(np.random.rayleigh(50, d[40:60].shape)),np.eye(4)), "noise.nii.gz")'
 
 # Test on example dataset
 # nlsam_denoising dwi_crop.nii.gz dwi_nlsam.nii.gz 1 bvals bvecs 5 -f --noise_est local_std --sh_order 0 --log log
@@ -28,20 +29,20 @@ python -c 'import nibabel as nib; import numpy as np; d = nib.load("mask.nii.gz"
 # nlsam_denoising dwi_crop.nii.gz dwi_nlsam.nii.gz 1 bvals bvecs 5 -m mask_crop.nii.gz -f --noise_est local_std --sh_order 0 --block_size 4,4,4 --save_sigma sigma.nii.gz
 # check_return_code $?
 
-nlsam_denoising dwi_crop.nii.gz dwi_nlsam.nii.gz 1 bvals bvecs 5 -m mask_crop.nii.gz -f --sh_order 0 --load_sigma sigma.nii.gz --no_subsample --fix_implausible
+# nlsam_denoising dwi_crop.nii.gz dwi_nlsam.nii.gz 1 bvals bvecs 5 -m mask_crop.nii.gz -f --sh_order 0 --load_sigma sigma.nii.gz --no_subsample --fix_implausible
+# check_return_code $?
+
+nlsam_denoising dwi_crop.nii.gz dwi_nlsam.nii.gz 1 bvals bvecs 5 -m mask_crop.nii.gz -f --sh_order 0 --noise_est noise_map --noise_map noise.nii.gz --cores 1 --noise_mask pmask.nii.gz
 check_return_code $?
 
-nlsam_denoising dwi_crop.nii.gz dwi_nlsam.nii.gz 1 bvals bvecs 5 -m mask_crop.nii.gz -f --sh_order 0 --noise_map sigma.nii.gz --cores 1 --noise_est piesno --noise_mask pmask.nii.gz
-check_return_code $?
-
-nlsam_denoising dwi_crop.nii.gz dwi_nlsam.nii.gz 1 bvals bvecs 5 -m mask_crop.nii.gz -f --sh_order 0 --noise_map sigma.nii.gz --no_stabilization --no_denoising
+nlsam_denoising dwi_crop.nii.gz dwi_nlsam.nii.gz 1 bvals bvecs 5 -m mask_crop.nii.gz -f --sh_order 0 --noise_est noise_map --noise_map noise.nii.gz --no_stabilization --no_denoising
 check_return_code $?
 
 # Test on niftis
 gunzip dwi_crop.nii.gz mask_crop.nii.gz
 
-nlsam_denoising dwi_crop.nii.gz dwi_nlsam.nii.gz 1 bvals bvecs 5 -m mask_crop.nii.gz -f --sh_order 0 --noise_map sigma.nii.gz --no_stabilization --load_sigma sigma.nii.gz --is_symmetric
+nlsam_denoising dwi_crop.nii.gz dwi_nlsam.nii.gz 1 bvals bvecs 5 -m mask_crop.nii.gz -f --sh_order 0 --no_stabilization --load_sigma sigma.nii.gz --is_symmetric
 check_return_code $?
 
-nlsam_denoising dwi_crop.nii.gz dwi_nlsam.nii.gz 1 bvals bvecs 5 -m mask_crop.nii.gz -f --sh_order 0 --noise_map sigma.nii.gz --no_denoising --save_sigma sigma.nii.gz --save_stab stab.nii.gz
+nlsam_denoising dwi_crop.nii.gz dwi_nlsam.nii.gz 1 bvals bvecs 5 -m mask_crop.nii.gz -f --sh_order 0 --no_denoising --save_sigma sigma.nii.gz --save_stab stab.nii.gz
 check_return_code $?
