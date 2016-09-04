96656c00b0a990a7224d7d02ad15571ae9e0583b
Samuel St-Jean <samuel@isi.uu.nl>
2016-09-05 00:03:26 +0200

    Added spec file and build script

--- build_stuff/linux/run_build.sh
+++
@@ -1,7 +0,0 @@
-source ~/buildfarm/bin/activate
-cd ~/nlsam
-pwd
-python setup.py build_ext -i --force
-pyinstaller build_stuff/linux/nlsam.spec --onefile
-pyinstaller build_stuff/linux/stabilizer.spec --onefile
-zip -j nlsam_linux_x64.zip dist/nlsam dist/stabilizer LICENSE README.md CHANGELOG.md
