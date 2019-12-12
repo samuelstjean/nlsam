c3376fdbd6b77d9d02ade5f6c0b5a0a0e2594d67
Samuel St-Jean <samuel.st-jean@usherbrooke.ca>
2019-12-12 14:54:36 +0100

    fix setup and appveyor versions

--- nlsam/__init__.py
+++ nlsam/__init__.py
@@ -10,7 +10,7 @@ def get_setup_params():
                                   'scipy>=0.19.1',
                                   'cython>=0.21',
                                   'nibabel>=2.0',
-                                  'joblib>=0.14.1'
+                                  'joblib>=0.14.1',
                                   'spams>=2.4']
     params['dependency_links'] = ['https://github.com/samuelstjean/spams-python/releases/download/0.1/spams-2.6.zip#egg=spams-2.6']
 
