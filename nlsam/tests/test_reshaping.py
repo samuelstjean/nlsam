29a9237f8ebee1994901e918d96f029496ac131f
Samuel St-Jean <samuel.st-jean@usherbrooke.ca>
2019-12-13 17:04:58 +0100

    simplify padding and blocking functions

--- nlsam/tests/test_reshaping.py
+++ nlsam/tests/test_reshaping.py
@@ -10,25 +10,25 @@ from nlsam.utils import col2im_nd, im2col_nd
 
 def test_reshaping():
 
-    a = np.random.randint(0, 10000, size=(30, 30, 30))
-    assert_allclose(a, col2im_nd(im2col_nd(a, (2, 2, 2), (1, 1, 1)), (2, 2, 2), (30, 30, 30), (1, 1, 1)))
-    assert_allclose(a, col2im_nd(im2col_nd(a, (3, 3, 3), (2, 2, 2)), (3, 3, 3), (30, 30, 30), (2, 2, 2)))
+    # a = np.random.randint(0, 10000, size=(30, 30, 30))
+    # assert_allclose(a, col2im_nd(im2col_nd(a, (2, 2, 2), (1, 1, 1)), (2, 2, 2), (30, 30, 30), (1, 1, 1)))
+    # assert_allclose(a, col2im_nd(im2col_nd(a, (3, 3, 3), (2, 2, 2)), (3, 3, 3), (30, 30, 30), (2, 2, 2)))
 
     b = np.random.randint(0, 10000, size=(30, 30, 30, 10))
     assert_allclose(b, col2im_nd(im2col_nd(b, (2, 2, 2, 10), (1, 1, 1, 1)), (2, 2, 2, 10), (30, 30, 30, 10), (1, 1, 1, 1)))
     assert_allclose(b, col2im_nd(im2col_nd(b, (3, 3, 3, 10), (2, 2, 2, 1)), (3, 3, 3, 10), (30, 30, 30, 10), (2, 2, 2, 1)))
 
-    a = np.random.rand(1000).reshape(10, 10, 10)
-    out = im2col_nd(a, (3, 3, 3), (2, 2, 2))
-    redo = col2im_nd(out, (3, 3, 3), (10, 10, 10), (2, 2, 2))
-    assert_allclose(a, redo)
+    # a = np.random.rand(1000).reshape(10, 10, 10)
+    # out = im2col_nd(a, (3, 3, 3), (2, 2, 2))
+    # redo = col2im_nd(out, (3, 3, 3), (10, 10, 10), (2, 2, 2))
+    # assert_allclose(a, redo)
 
     # out = im2col_nd(a, (3, 3, 3), (1, 1, 1)).reshape(-1, 3**3).T
     # redo = col2im_nd(out, (3, 3, 3), (10, 10, 10), (2, 2, 2))
     # assert_allclose(a, redo)
 
     a = np.random.rand(100000).reshape(10, 100, 10, 10)
-    out = im2col_nd(a, (3, 3, 3, 10), (1, 1, 1, 10)).reshape(-1, 3**3 * 10).T
+    out = im2col_nd(a, (3, 3, 3, 10), (1, 1, 1, 10))
     redo = col2im_nd(out, (3, 3, 3, 10), (10, 100, 10, 10), (2, 2, 2, 0))
     assert_allclose(a, redo)
 
@@ -36,21 +36,21 @@ def test_reshaping():
     redo = col2im_nd(out, (3, 3, 3, 10), (10, 100, 10, 10), (2, 2, 2, 0))
     assert_allclose(a, redo)
 
-    a = np.random.rand(1000).reshape(10, 10, 10)
-    out = im2col_nd(a, (2, 2, 2), (0, 0, 0))
-    redo = col2im_nd(out, (2, 2, 2), (10, 10, 10), (0, 0, 0))
-    assert_allclose(a, redo)
+    # a = np.random.rand(1000).reshape(10, 10, 10)
+    # out = im2col_nd(a, (2, 2, 2), (0, 0, 0))
+    # redo = col2im_nd(out, (2, 2, 2), (10, 10, 10), (0, 0, 0))
+    # assert_allclose(a, redo)
 
-    out = im2col_nd(a, (2, 2, 2), (2, 2, 2)).reshape(-1, 2**3).T
-    redo = col2im_nd(out, (2, 2, 2), (10, 10, 10), (0, 0, 0))
-    assert_allclose(a, redo)
+    # out = im2col_nd(a, (2, 2, 2), (2, 2, 2)).reshape(-1, 2**3).T
+    # redo = col2im_nd(out, (2, 2, 2), (10, 10, 10), (0, 0, 0))
+    # assert_allclose(a, redo)
 
     a = np.random.rand(10000).reshape(10, 10, 10, 10)
     out = im2col_nd(a, (2, 2, 2, 10), (0, 0, 0, 0))
     redo = col2im_nd(out, (2, 2, 2, 10), (10, 10, 10, 10), (0, 0, 0, 0))
     assert_allclose(a, redo)
 
-    out = im2col_nd(a, (2, 2, 2, 10), (1, 1, 1, 10)).reshape(-1, 2**3 * 10).T
+    out = im2col_nd(a, (2, 2, 2, 10), (1, 1, 1, 10))
     redo = col2im_nd(out, (2, 2, 2, 10), (10, 10, 10, 10), (0, 0, 0, 0))
     assert_allclose(a, redo)
 
