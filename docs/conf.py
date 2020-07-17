ec1b588d07cc88e810214de14943abf418421009
Samuel St-Jean <samuel@isi.uu.nl>
2020-07-17 10:13:12 -0400

    rtd skip test_ only pattern

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ extensions = ['autoapi.extension',
 
 autoapi_type = 'python'
 autoapi_dirs = ['../nlsam']
-autoapi_ignore = ['*test*']
+autoapi_ignore = ['test_*']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
