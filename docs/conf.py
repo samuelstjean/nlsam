45396e2bc026819ff527a372e3a61951bdc6ca03
Samuel St-Jean <samuel@isi.uu.nl>
2020-07-17 10:46:11 -0400

    recommonmark update

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ extensions = ['autoapi.extension',
 
 autoapi_type = 'python'
 autoapi_dirs = ['../nlsam']
-autoapi_ignore = ["*/test_*.py"]
+autoapi_ignore = ["*/tests/test_*.py"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
