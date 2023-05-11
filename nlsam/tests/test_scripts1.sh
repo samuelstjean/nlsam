b6041390aafa5f3b0066c6175c8202223819f371
Samuel <3030760+samuelstjean@users.noreply.github.com>
2023-05-11 14:44:50 +0200

    pimp setup.py

--- nlsam/tests/test_scripts1.sh
+++ nlsam/tests/test_scripts1.sh
@@ -29,7 +29,7 @@ check_return_code $?
 # Test on niftis
 gunzip dwi_crop.nii.gz mask_crop.nii.gz sigma.nii.gz
 
-nlsam_denoising dwi_crop.nii dwi_nlsam.nii bvals bvecs -m mask_crop.nii -f --verbose --sh_order 0 --no_stabilization --load_sigma sigma.nii --is_symmetric --use_threading --save_difference diff.nii
+nlsam_denoising dwi_crop.nii dwi_nlsam.nii bvals bvecs -m mask_crop.nii -f --verbose --sh_order 0 -N 1 --no_stabilization --load_sigma sigma.nii --is_symmetric --use_threading --save_difference diff.nii
 check_return_code $?
 
 nlsam_denoising dwi_crop.nii dwi_nlsam.nii bvals bvecs -m mask_crop.nii -f --verbose --sh_order 0 --no_denoising --save_sigma sigma.nii --save_stab stab.nii --load_mhat dwi_crop.nii --save_eta eta.nii
