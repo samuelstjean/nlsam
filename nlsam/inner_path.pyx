feb1cbce47848128d1f63304def815d1fb35693e
Samuel <3030760+samuelstjean@users.noreply.github.com>
2023-10-27 15:24:33 +0200

    more qr and index stuff

--- nlsam/inner_path.pyx
+++ nlsam/inner_path.pyx
@@ -402,6 +402,8 @@ cdef inline void make_diag_pos(double[:, :] Q, double[:, :] R) noexcept nogil:
 
 #     return out
 
+# def print(*a):
+#     pass
 
 def inner_path(np.ndarray[double, ndim=2] X,
                np.ndarray[double, ndim=2] y,
@@ -419,7 +421,7 @@ def inner_path(np.ndarray[double, ndim=2] X,
         int m = D.shape[0]
         int i0_local, k, splits, df, idx, r, cutoff, nextidx, coord
         double lambdak, lk, hk
-        np.ndarray[np.int64_t, ndim=1] indices
+        # np.ndarray[np.int64_t, ndim=1] indices
 
     # np.int16_t[:] K = np.zeros(N, dtype=np.int16)
     XtX = X.T @ X
@@ -448,12 +450,10 @@ def inner_path(np.ndarray[double, ndim=2] X,
     all_dfs = np.zeros((N, nsteps), dtype=np.int16)
     all_Ks = np.zeros(N, dtype=np.int16)
 
-
-    Xmean = X.mean(axis=1, keepdims=True)
-    Xty_mean = Xty.mean(axis=1, keepdims=True)
-
-    Q_orig, R_orig = np.linalg.qr(Xmean)
-    lmin = max(lmin, eps)
+    # Xmean = X.mean(axis=1, keepdims=True)
+    Xty_mean = Xty.mean(axis=0, keepdims=True)
+    Q_orig, R_orig = np.linalg.qr(X.mean(axis=1, keepdims=True))
+    # lmin = max(lmin, eps)
 
     for i in range(N):
         B[:] = 0
@@ -494,21 +494,23 @@ def inner_path(np.ndarray[double, ndim=2] X,
         lambdas = all_lambdas[i]
         betas = all_betas[i]
         dfs = all_dfs[i]
-        # K = all_Ks[i]
         lambdas[0] = h0[i]
-        # df = 1
         r = 1
         cutoff = i0_local
 
-        XH_prev = Xmean
-        HtXty_prev = Xty_mean[i]
-        HtDts_prev = np.zeros(1)
+        # XH_prev = X.mean(axis=1, keepdims=True)
+
+        nsplits_prev = 0
+        HtXty = Xty_mean[:, i]
+        HtDts = np.zeros(1)
 
         Q = Q_orig
         R = R_orig
 
+        print('initial cutoff', cutoff, np.nonzero(~B)[0])
+
         while lambdas[k] > lmin and k < (nsteps - 1):
-            # print(k, lambdas[k])
+            print(k, lambdas[k])
 
             Db = D[B]
             Dm = D[~B]
@@ -517,77 +519,159 @@ def inner_path(np.ndarray[double, ndim=2] X,
 
             consec = np.split(all_indexes, np.nonzero(B)[0] + 1)
             nsplits = len(consec)
+            nsplits1 = B.sum() + 1
+            # print(nsplits, nsplits1)
 
-            XH = np.zeros((X.shape[0], nsplits))
-            HtXty = np.zeros(nsplits)
-            HtDts = np.zeros(nsplits)
+            # # ################
+            # XH_old = np.zeros((X.shape[0], nsplits))
+            # # HtXty_old = np.zeros(nsplits)
+            # # HtDts_old = np.zeros(nsplits)
 
-            iterator = enumerate(consec)
-            nextidx = 0
+            # iterator = enumerate(consec)
+            # nextidx = 0
 
-            for idx, indices in iterator:
-                # print(k, idx ,indices, cutoff, consec)
-                if indices[-1] == cutoff: # new cutoff point in the graph
-                    cmean2(X[:, indices], XH[:, idx])
-                    HtXty[idx] = cmean(Xty_local[indices])
-                    HtDts[idx] = cmean(Dts[indices])
+            # for idx, indices in iterator:
+            #     # print(k, idx ,indices, cutoff, consec)
+            #     if indices[-1] == cutoff: # new cutoff point in the graph
+            #         cmean2(X[:, indices], XH_old[:, idx])
+            #         # HtXty_old[idx] = cmean(Xty_local[indices])
+            #         # HtDts_old[idx] = cmean(Dts[indices])
 
-                    # process the next group as it also changed if we are not at the end
-                    idx, indices = next(iterator, (None, None))
-                    if idx is not None:
-                        cmean2(X[:, indices], XH[:, idx])
-                        HtXty[idx] = cmean(Xty_local[indices])
-                        HtDts[idx] = cmean(Dts[indices])
+            #         # process the next group as it also changed if we are not at the end
+            #         idx, indices = next(iterator, (None, None))
+            #         if idx is not None:
+            #             cmean2(X[:, indices], XH_old[:, idx])
+            #             # HtXty_old[idx] = cmean(Xty_local[indices])
+            #             # HtDts_old[idx] = cmean(Dts[indices])
 
-                        # we now have to copy the next index from the old array to the new array
-                        # go back one if we add a new column to the new array
-                        # skip one extra instead if we merge two columns in the new array
-                        if nsplits > XH_prev.shape[1]:
-                            nextidx = -1
-                        else:
-                            nextidx = 1
+            #             # we now have to copy the next index from the old array to the new array
+            #             # go back one if we add a new column to the new array
+            #             # skip one extra instead if we merge two columns in the new array
+            #             if nsplits > XH_prev.shape[1]:
+            #                 nextidx = -1
+            #             else:
+            #                 nextidx = 1
 
-                else: # copy the old data if it didn't change
-                    XH[:, idx] = XH_prev[:, idx+nextidx]
-                    HtXty[idx] = HtXty_prev[idx+nextidx]
-                    HtDts[idx] = HtDts_prev[idx+nextidx]
+            #     else: # copy the old data if it didn't change
+            #         XH_old[:, idx] = XH_prev[:, idx+nextidx]
+            #         # HtXty_old[idx] = HtXty_prev[idx+nextidx]
+            #         # HtDts_old[idx] = HtDts_prev[idx+nextidx]
 
-            # Q, R = np.linalg.qr(XH_prev)
+            # # ################
 
-            iterator = enumerate(consec)
-            for idx, indices in iterator:
-                if indices[-1] == cutoff: # new cutoff point in the graph
-                    if nsplits > XH_prev.shape[1]:
-                        addcol = 2
-                        delcol = 1
-                    else:
-                        addcol = 1
-                        delcol = 2
-                        _, indices = next(iterator)
-                        idx += 1 # idx is the column before the two columns that will merge this step
-                        # print('new indices', indices)
+            # Q, R = np.linalg.qr(XH_old)
+            # XH_prev = XH_old
+            # print('shapes', Q.shape, R.shape, XH_old.shape)
+            # iterator = enumerate(consec)
+            # for idx, indices in iterator:
+            #     if indices[-1] == cutoff: # new cutoff point in the graph
 
-                    newXH = np.zeros((X.shape[0], addcol))
-                    cmean2(X[:, indices], newXH[:, 0])
+            # raise ValueError()
 
-                    if addcol == 2:
-                        _, indices = next(iterator)
-                        cmean2(X[:, indices], newXH[:, 1])
+            print('consec', consec)
+            all_indices = np.nonzero(B)[0]
+            print('indices', all_indices, cutoff)
+            idx = np.nonzero(all_indices == cutoff)[0]
+            # all_indices += 1
+            # print(consec)
+            # print(nsplits, nsplits_prev)
+            # print(all_indices, idx, cutoff, all_indices.shape, all_indices.ndim)
+            # raise ValueError()
+            if nsplits > nsplits_prev:
+                addcol = 2
+                delcol = 1
+                if idx == 0:
+                    indices = slice(0, all_indices[idx] + 1)
+                else:
+                    indices = slice(all_indices[idx - 1] + 1, all_indices[idx] + 1)
+                # slice on the right
+                # if idx == 0:
+                #     indices = slice(0, all_indices[idx] + 1)
+                # # elif idx == len(all_indices) - 1:
+                #     # indices = slice(all_indices[idx-1] + 1, m + 1)
+                # else:
+                #     indices = slice(all_indices[idx-1] + 1, all_indices[idx] + 1)
+            else:
+                addcol = 1
+                delcol = 2
+                print('in delcol', addcol, delcol, idx, cutoff ,all_indices)
+                # if all_indices[idx] == 1:
+                    # indices = slice(0, all_indices[0])
+                if idx == 0:
+                    indices = slice(0, all_indices[0] + 1)
+                else:
+                    indices = slice(all_indices[idx - 1] + 1, all_indices[idx] + 1)
+                # slice on the left
+                # if idx == 0:
+                    # indices = slice(0, all_indices[idx] + 1)
+                # idx += 1
 
-                    # print('QR update', k, idx ,indices, cutoff, addcol, delcol, consec)
-                    # print('QR update', Q.shape, R.shape)
-                    Q, R = la.qr_delete(Q, R, idx, p=delcol, which='col', check_finite=False)
-                    # print('QR update', Q.shape, R.shape)
-                    Q, R = la.qr_insert(Q, R, newXH, idx, which='col', overwrite_qru=False, check_finite=False)
-                    # print('QR update', Q.shape, R.shape)
+            # print(idx, all_indices, cutoff, len(all_indices))
 
-                    # If Q is square, the downdate will change to full mode instead of economic
-                    # so we strip the (possible) zeros here to make R square again
-                    if R.shape[1] < R.shape[0]:
-                        # print('resize', Q.shape, R.shape)
-                        R = R[:R.shape[1], :R.shape[1]]
-                        Q = Q[:, :R.shape[1]]
-                        # print('resize', Q.shape, R.shape)
+
+            # print('current', indices, idx, all_indices)
+
+
+                # _, indices = next(iterator)
+                # print('new indices', indices)
+
+            newXH = np.zeros((X.shape[0], addcol))
+
+            cmean2(X[:, indices], newXH[:, 0])
+            new_HtXty = [cmean(Xty_local[indices])]
+            new_HtDts = [cmean(Dts[indices])]
+
+            if addcol == 2:
+                print('previous set', indices)
+                # _, indices = next(iterator)
+                # slice on the right
+                # if idx == 0:
+                #     indices = slice(0, all_indices[idx] + 1)
+                if idx == len(all_indices) - 1:
+                    indices = slice(all_indices[idx] + 1, m + 1)
+                else:
+                    indices = slice(all_indices[idx] + 1, all_indices[idx+1] + 1)
+                cmean2(X[:, indices], newXH[:, 1])
+                new_HtXty.append(cmean(Xty_local[indices]))
+                new_HtDts.append(cmean(Dts[indices]))
+
+            if delcol == 2:
+                todel = (idx, idx + 1)
+                toins = idx
+            else:
+                todel = (idx,)
+                toins = idx
+
+            print('newXH', newXH)
+            print('QR', Q@R)
+            print('new_HtXty', new_HtXty)
+            # print(new_HtDts)
+            print(cutoff, indices, all_indices, idx, consec, todel, toins)
+            print('QR update', k, idx ,indices, cutoff, addcol, delcol, consec)
+            print('QR update', Q.shape, R.shape, idx, delcol, cutoff)
+            Q, R = la.qr_delete(Q, R, todel[0], p=delcol, which='col', check_finite=False)
+            print('QR update', Q.shape, R.shape)
+            Q, R = la.qr_insert(Q, R, newXH, toins, which='col', overwrite_qru=False, check_finite=False)
+            print('QR update', Q.shape, R.shape)
+            print('QR updated', Q@R)
+
+            print('before del', todel, idx, addcol, delcol, HtXty.shape, HtDts.shape)
+            print(HtXty)
+            HtXty = np.delete(HtXty, todel)
+            HtDts = np.delete(HtDts, todel)
+
+            HtXty = np.insert(HtXty, toins, new_HtXty)
+            HtDts = np.insert(HtDts, toins, new_HtDts)
+            print('after del', todel, toins, idx, addcol, delcol, HtXty.shape, HtDts.shape)
+            print(HtXty, '\n')
+
+            # If Q is square, the downdate will change to full mode instead of economic
+            # so we strip the (possible) zeros here to make R square again
+            if R.shape[1] < R.shape[0]:
+                # print('resize', Q.shape, R.shape)
+                R = R[:R.shape[1], :R.shape[1]]
+                Q = Q[:, :R.shape[1]]
+                # print('resize', Q.shape, R.shape)
 
 
             # diagneg = np.diagonal(R) < 0
@@ -595,11 +679,15 @@ def inner_path(np.ndarray[double, ndim=2] X,
             # R[diagneg] *= -1
 
             # print(XH_prev)
-            # print(XH)
+            # print(XH_old)
             # print(Q@R)
-            XH_prev = XH
-            HtXty_prev = HtXty
-            HtDts_prev = HtDts
+            # XH_prev = XH_old
+            # HtXty_prev = HtXty_old
+            # HtDts_prev = HtDts_old
+            nsplits_prev = nsplits
+
+            # HtXty = HtXty_old
+            # HtDts = HtDts_old
 
             # print('diff QR', np.abs(XH.T@XH - R.T@R).max(), np.abs(XH - Q@R).max(), np.shape(XH), np.shape(Q), R.shape)
             # print('Q', Q)
@@ -645,20 +733,23 @@ def inner_path(np.ndarray[double, ndim=2] X,
             # Q[:, diagneg] *= -1
             # R[diagneg] *= -1
             # print('R pos', np.diag(R), R.shape, XH.shape)
-
+            # print('Ht', HtXty)
+            # print('Ht', HtDts)
+            # print('R.T@R', R.shape, R.T@R)
             A1 = cho_solve(R, HtXty)
             A2 = cho_solve(R, HtDts)
+            # A1 = np.linalg.lstsq(R.T @ R, HtXty, rcond=-1)[0]
+            # A2 = np.linalg.lstsq(R.T @ R, HtDts, rcond=-1)[0]
+            # print('A1', np.asarray(A1))
+            # print('A2', np.asarray(A2))
+            # print('R', np.diag(R), R.shape)
 
-            # try:
-            #     H = la.null_space(Dm)
+            HA1 = np.zeros(p)
+            HA2 = np.zeros(p)
 
-            #     A1b = np.linalg.lstsq((X@H).T @ (X@H), H.T @ Xty_local, rcond=-1)[0]
-            #     A2b = np.linalg.lstsq((X@H).T @ (X@H), H.T @ Dts, rcond=-1)[0]
-
-            #     print('diff', np.abs(A1 - A1b).max(), np.abs(H.T @ Xty_local - HtXty).max(), np.abs((X@H).T @ (X@H) - R.T@R).max())
-            #     print('diff', np.abs(A2 - A2b).max(), np.abs(H.T @ Dts - HtDts).max())
-            # except:
-            #     print('end reached')
+            for idx, indices in enumerate(consec):
+                HA1[indices] = A1[idx] / len(indices)
+                HA2[indices] = A2[idx] / len(indices)
 
             # step 3a
             if r == m: # Interior is empty
@@ -668,7 +759,8 @@ def inner_path(np.ndarray[double, ndim=2] X,
             else:
                 DDt = Dm @ Dm.T
                 DDt_banded = diagonal_choform(DDt)
-                XtXHA = X.T @ XH @ np.vstack((A1, A2)).T
+                # XtXHA = X.T @ Q @ R @ np.vstack((A1, A2)).T
+                XtXHA = XtX @ np.vstack((HA1, HA2)).T
 
                 L_banded = cholesky_banded(DDt_banded)
                 rhs = np.vstack((Xty_local, Dts)).T - XtXHA
@@ -700,7 +792,7 @@ def inner_path(np.ndarray[double, ndim=2] X,
                 t = a / (b + np.sign(a))
                 # numerical issues fix
                 t[t > lambdas[k] + eps] = 0
-
+                # t[t >= lambdas[k]] = 0
                 ik = np.argmax(t)
                 hk = t[ik]
 
@@ -718,12 +810,6 @@ def inner_path(np.ndarray[double, ndim=2] X,
             # HA1 = H @ A1
             # HA2 = H @ A2
 
-            HA1 = np.zeros(p)
-            HA2 = np.zeros(p)
-
-            for idx, indices in enumerate(consec):
-                HA1[indices] = A1[idx] / len(indices)
-                HA2[indices] = A2[idx] / len(indices)
 
             if r == 0: # or HA1.shape[0] == HA1.shape[1]: # Boundary is empty
                 lk = 0
@@ -736,6 +822,7 @@ def inner_path(np.ndarray[double, ndim=2] X,
                 # tl = np.divide(c, d, where=(c < 0) & (d < 0), out=np.zeros_like(c))
                 # numerical issues fix
                 tl[tl > lambdas[k] + eps] = 0
+                # tl[tl >= lambdas[k]] = 0
 
                 ilk = np.argmax(tl)
                 lk = tl[ilk]
@@ -743,14 +830,24 @@ def inner_path(np.ndarray[double, ndim=2] X,
                 if lk < lmin:
                     lk = 0
 
+                # print('c', c)
+                # print('d', d)
+                # print('tl', tl)
+
+            # print('hk', hk)
+            # print(t)
+            # print('lk', lk)
+            # print(tl)
             # update matrices and stuff for next step
             if hk > lk: # variable enters the boundary
                 coord = np.nonzero(~B)[0][ik]
-                updates = True, np.sign(a[ik])
+                update_B = True
+                update_S = np.sign(a[ik])
                 lambdak = hk
                 # graph.delete_edges([(coord, coord + 1)])
                 cutoff = coord
                 r += 1
+                print('add cutoff', coord, cutoff, ik, np.nonzero(~B)[0])
 
                 # newH[:] = 1
                 # newH[:coord + 1] = 0
@@ -769,9 +866,19 @@ def inner_path(np.ndarray[double, ndim=2] X,
                 # # print(np.abs(Q1@R1 - XH).sum(), np.sum(np.diag(R1) < 0), XH.shape, Q1.shape, R1.shape)
             elif lk > hk: # variable leaves the boundary
                 coord = np.nonzero(B)[0][ilk]
-                updates = False, 0
+                update_B = False
+                update_S = 0
                 lambdak = lk
-                cutoff = np.nonzero(B)[0][ilk-1]
+                print('remove cutoff', coord, lk, ilk, np.nonzero(B)[0])
+
+                # Next index is our cutoff on the right because columns will merge
+                # if we are already on the last item then it's our cutoff
+                if ilk == len(np.nonzero(B)[0]) - 1:
+                    cutoff = np.nonzero(B)[0][ilk - 1]
+                else:
+                    cutoff = np.nonzero(B)[0][ilk + 1]
+                # cutoff = coord
+                print('remove cutoff', coord, cutoff, ilk, np.nonzero(B)[0])
                 # graph.add_edges([(coord, coord + 1)])
                 r -= 1
                 # print('remove', r, ilk, coord)
@@ -788,7 +895,8 @@ def inner_path(np.ndarray[double, ndim=2] X,
                 # Can be triggered if lambda = 0 is the only and first solution
                 lambdak = 0
                 coord = 0
-                updates = False, 0
+                update_B = False
+                update_S = 0
 
             # Ensure diag(R) is always positive for cholesky by flipping signs of negative entries in QR
             # Q1 = Q.copy()
@@ -809,8 +917,8 @@ def inner_path(np.ndarray[double, ndim=2] X,
             mus[k] = muk
 
             # update boundaries
-            B[coord] = updates[0]
-            S[coord] = updates[1]
+            B[coord] = update_B
+            S[coord] = update_S
             betas[k] = HA1 - lambdak * HA2 # eq. 38
             lambdas[k] = lambdak
             dfs[k] = nsplits
