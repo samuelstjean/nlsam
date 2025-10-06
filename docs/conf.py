cbb2c461816f30dd5e1e68daa076d1b265c46c5b
Samuel St-Jean <3030760+samuelstjean@users.noreply.github.com>
2025-10-06 13:14:09 -0400

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -59,9 +59,9 @@ author = 'Samuel St-Jean'
 # built documents.
 #
 # The short X.Y version.
-version = '0.7.2'
+version = '0.7.3'
 # The full version, including alpha/beta/rc tags.
-release = '0.7.2'
+release = '0.7.3'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
